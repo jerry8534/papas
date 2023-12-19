a4049f060800334f3a426e8bc6ddd580bdf04f3f
jerry8534 <jerryjjinu@gmail.com>
2023-12-19 23:16:25 +0900

    Java Init

---
+++ fw/src/test/java/fw/fw/FwApplicationTests.java
@@ -0,0 +1,13 @@
+package fw.fw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
